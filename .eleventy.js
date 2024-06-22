1a14185d63505f7ebb8419eb9c2577d1ebd9f843
leah <leah@pronounmail.com>
2024-06-22 20:57:26 +0100

    h

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,18 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy(".well-known");
+
+    eleventyConfig.addShortcode('renderblock', function (name) {
+        return (this.page.layoutblock || {})[name] || '';
+    });
+
+    eleventyConfig.addPairedShortcode('defblock', function (content, name) {
+        if (!this.page.layoutblock) this.page.layoutblock = {};
+        this.page.layoutblock[name] = content;
+        return '';
+    });
+
+    return {
+        markdownTemplateEngine: "njk",
+    }
 };
